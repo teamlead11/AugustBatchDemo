f779c298f35c54651cee53000cf8fbb6d9aca08e
Saradha1 <k.saradhamani@gmail.com>
2018-08-21 12:45:14 +0530

    Added changes in Framework.java in line no9

--- src/testpack/Framework.java
+++ src/testpack/Framework.java
@@ -5,6 +5,8 @@ public class Framework {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my framework");
+		//Added by Saradha on 21/8/2018
+		System.out.println(August batch test);
 	}
 
 }
